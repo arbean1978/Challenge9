240e8f971627a24111d636c09c42468a0a9a41f6
Aaron Bean <arbean1978@gmail.com>
2022-01-16 11:23:34 -0700

    generateMarkdown

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -12,8 +12,29 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  return `
+  ![GitHub](https://img.shields.io/github/license/${data.githubUsername}/${data.title})
+  # ${data.title}
+  ## Description
+  ${data.about}
+  ## Table of Contents
+  ${installCheck(data.install)}
+  * [Usage] (#usage)
+  * [License] (#license)
+  ${contributeCheck(data.contribution)}
+  ${testCheck(data.testing)}
+  * [Questions] (#questions)
+  ${generateInstall(data.install)}
+  ## Usage
+  ${data.usage}
+  ## License
+  * This application is covered under the ${data.license} license
+  ${generateContribute(data.contribution)}
+  ${generateTesting(data.testing)}
+  ## Questions
+  Created by: [${data.githubUsername}] (${data.gitLink})
+  If you have further questions contact me at [${data.email}] (${data.email})  
+  `;
 }
 
 module.exports = generateMarkdown;
